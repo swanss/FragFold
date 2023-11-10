a5cb548609dd1b2069c63c7b89e8bc5d560a94f5
Sebastian Swanson <swans@mit.edu>
2023-11-09 23:46:21 -0500

    Update run_colabfold_process_output.sh to work with new installation

--- fragfold/submit_jobs/run_colabfold_process_output.sh
+++ fragfold/submit_jobs/run_colabfold_process_output.sh
@@ -6,17 +6,24 @@
 #SBATCH --mem=8G
 #SBATCH -o process_colabfold_output.%j.log
 
-CONDA_ROOT=/state/partition1/llgrid/pkg/anaconda/anaconda3-2022b/
-source ${CONDA_ROOT}/etc/profile.d/conda.sh
-conda activate alphafold_fragment_prediction
+### -------------------------------------------------------------------- ###
+# Set the following parameters
 
-REPO=/data1/groups/keatinglab/swans/savinovCollaboration/inhibitory_fragments_structure_prediction
+# Environments and installation paths
+repo_dir=/data1/groups/keatinglab/swans/savinovCollaboration/FragFold
+conda_env_name=fragfold
+
+# Inputs 
+import_json= # path to JSON file with colabfold output directories
+experimental_data=$repo_dir/input/data/Savinov_2022_inhib_peptide_mapping.csv
+
+### -------------------------------------------------------------------- ###
 
 SECONDS=0
 
-python $REPO/inhib_frag_pred/colabfold_process_output.py \
-    --import_json $REPO/input/json/colabfold115_output.json \
-    --experimental_data $REPO/input/data/Savinov_2022_inhib_peptide_mapping.csv
+conda run --no-capture-output -n $conda_env_name python $REPO/fragfold/colabfold_process_output.py \
+    --import_json $import_json \
+    --experimental_data $experimental_data
 
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
