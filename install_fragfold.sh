66c86b9dc45067ed43dbc51d1405f7c3ca035152
Sebastian Swanson <swans@mit.edu>
2024-06-24 21:25:49 -0400

    change name of env to fragfold

--- install_fragfold.sh
+++ install_fragfold.sh
@@ -2,7 +2,7 @@
 
 source $(conda info --base)/etc/profile.d/conda.sh
 
-conda create -n fragfold5 -c bioconda python==3.9 nextflow -y && \
-    conda activate fragfold5 && \
+conda create -n fragfold -c bioconda python==3.9 nextflow -y && \
+    conda activate fragfold && \
     pip install . && \
     echo "Installation complete"
